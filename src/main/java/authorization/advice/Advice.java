e39230d68fd9aca16eb98ad43c78e723b0b74c8e
Aleksandr Koloshtivin <alexkalash2012@gmail.com>
2022-07-19 09:51:12 +0200

    Validation dependency & ConstraintViolationException handler have been added

--- src/main/java/authorization/advice/Advice.java
+++ src/main/java/authorization/advice/Advice.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolationException;
+
 @RestControllerAdvice("authorization")
 public class Advice {
     @ExceptionHandler(InvalidCredentials.class)
@@ -18,6 +20,14 @@ public class Advice {
     @ExceptionHandler(UnauthorizedUser.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     String resolveException(UnauthorizedUser ex) {
+        final var msg = ex.getMessage();
+        System.err.println(msg);
+        return msg;
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    String resolveException(ConstraintViolationException ex) {
         final var msg = ex.getMessage();
         System.err.println(msg);
         return msg;
